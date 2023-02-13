deb80834425202160b03c90439febb0ee6f14ed9
juan.amaya <juan.amaya@sofka.com.co>
2023-02-13 10:21:49 -0500

    all homework to simulation

---
+++ 03.js
@@ -0,0 +1,31 @@
+/* 
+  Importante: 
+  No modificar ni el nombre ni los argumetos que reciben las funciones, sólo deben escribir
+  código dentro de las funciones ya definidas. 
+  No comentar la funcion 
+*/
+function buscarAmigo(amigos, nombre) {
+  // La funcion llamada 'buscarAmigo' recibe como argumento un array llamado 'amigos' que contiene
+  // en cada posición del arreglo un objeto que tiene como propiedades 'nombre' y 'edad'. También
+  // recibe un string llamado 'nombre'.
+  // Debe devolver el objeto cuya propiedad 'nombre' coincida con el string 'nombre' recibido por argumento.
+  // Ej:
+  //  var amigos = [{ nombre: 'toni', edad: 33 } , { nombre: 'Emi', edad: 25 }];
+  //  buscarAmigo(amigos, 'toni') debe devolver { nombre: 'toni', edad: 33 };
+
+  // Tu código aca:
+  //return amigos.filter(x => x[nombre] == nombre);
+  
+/*
+    if(amigos[0].nombre === nombre ){
+      return amigos[0];
+    }else {
+      return amigos[1];
+    }
+
+*/
+}
+
+// No modifiques nada debajo de esta linea //
+
+module.exports = buscarAmigo
